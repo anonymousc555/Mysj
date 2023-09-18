576aaaef206d1b8513044b798372fa0a0f28e1ef
fomativeh <ufomaitive@gmail.com>
2023-09-18 06:14:43 +0100

    Added an image slider, removed background video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import InfiniteCarousel from 'react-leaf-carousel';
+import card1 from "../public/assets/images/card-1.png";
+import card2 from "../public/assets/images/card-2.jpg"
+import card3 from "../public/assets/images/card-3.png";
+import card4 from "../public/assets/images/card-4.png";
+import "../styles/Home.css"
 
 export default function Home() {
   return (
@@ -10,7 +16,64 @@ export default function Home() {
             <h1>Economic & Political</h1>
             <h3>Landscape Of South Eastern Nigeria</h3>
           </div>
-          <video playsInline autoPlay muted loop src="/video.mp4"></video>
+          {/* {/* <video playsInlisne autoPlay muted loop src="/video.mp4"></video>  */}
+        </div>
+        <div className="slider-wrapper">
+         <div className="inner-wrap">
+         <InfiniteCarousel
+            breakpoints={[
+              {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
+            ]}
+            dots={true}
+            showSides={true}
+            sidesOpacity={0.5}
+            sideSize={0.1}
+            slidesToScroll={2}
+            slidesToShow={2}
+            slidesSpacing={40}
+          >
+            <div>
+              <img
+                alt=""
+                src={card1}
+              />
+            </div>
+            <div>
+              <img
+                alt=""
+                src={card2}
+              />
+            </div>
+
+            <div>
+              <img
+                alt=""
+                src={card3}
+              />
+            </div>
+
+            <div>
+              <img
+                alt=""
+                src={card4}
+              />
+            </div>
+          
+          </InfiniteCarousel>
+         </div>
         </div>
         <div className="event-details">
           <div className="date-of-event">
@@ -24,7 +87,6 @@ export default function Home() {
             <h2>
               ALAIGBO - Economic <br /> Summit
             </h2>
-            {/* <h3>Economic Summit</h3> */}
             <button>
               <Link className="summit-btn" to="/form">
                 Get Your Ticket
