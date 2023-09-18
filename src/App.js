62382ca7343cd9c791e7fe0b3bf1d0555e225b17
Olumide Nifemi <olumidemichelle@gmail.com>
2023-09-17 19:42:20 -0700

    first page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import Info1 from './components/Info1'
 
 export default function App() {
   return (
     <>
       <Header />
       <Home />
+      <Info1 />
       <Footer />
     </>
   )
---
+++ src/components/Info1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from '../logo.png'
+
+function Info1() {
+  return (
+    <>
+    <main className='infoOne'>
+         <div className="logo-section">
+          <img src={Logo} alt="Logo" className='logo' />
+          <h2 className='sub fw-bolder'>ALAIGBO Youth Forum</h2>
+          <p className='fw-bold fs-4'>PRESENTS</p>
+          <div className="mainTxt">
+            <p className='fw-bold'>Alaigbo Youth Summit</p>
+            <img src="line.png" alt="" />
+            <span className='fw-bold subtxt'>
+              Redefining the Economic and Political Landscape of South Eastern Nigeria 
+            </span>
+          </div>
+        </div>
+    </main>
+    </>
+  )
+}
+
+export default Info1
