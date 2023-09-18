4bd45e57f6b24637ced746b2227ef1ed0e3c1ef4
Olumide Nifemi <olumidemichelle@gmail.com>
2023-09-17 20:10:30 -0700

    2nd page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer'
 import Info1 from './components/Info1'
+import InfoSec from './components/InfoSec'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <Home />
       <Info1 />
+      <InfoSec />
       <Footer />
     </>
   )
---
+++ src/components/InfoSec.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function InfoSec() {
+  return (
+    <>
+        <main className='info2'>
+            <h1 className='fw-bold objT'>Objectives</h1>
+            <ul>
+                <li className='fs-5 mt-3'>
+                Analyzing community
+                challenges and developing
+                effective solutions.
+                </li>
+                <li className='fs-5 mt-3'>
+                Creating a comprehensive
+                roadmap for the development
+                of Alaigbo.
+                </li>
+                <li className='fs-5 mt-3'>
+                Supporting growth and
+                development in our covered
+                communities.
+                </li>
+                <li className='fs-5 mt-3'>
+                Building a peaceful and
+                cohesive society while
+                preserving our Igbo cultural
+                heritage.
+                </li>
+                <li className='fs-5 mt-3'>
+                Engaging political leaders to
+                maximize their potential.
+                </li>
+                <li className='fs-5 mt-3'>
+                Positioning our indigenous
+                businesses on the world map
+                using international best
+                practices.
+                </li>
+            </ul>
+        </main>
+    </>
+  )
+}
+
+export default InfoSec
